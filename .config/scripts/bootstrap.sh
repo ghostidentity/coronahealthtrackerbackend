386dec598b6203e9bbd3231a783574fe31c57234
Mark Tagab <proxyshadow@gmail.com>
2020-03-17 22:38:51 +0800

    moved shell scripts

---
+++ .config/scripts/bootstrap.sh
@@ -0,0 +1,5 @@
+minikube --memory 3000 --cpus 2 start
+minikube addons enable ingress
+kubectl create clusterrolebinding admin --clusterrole=cluster-admin --serviceaccount=default:default
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/provider/baremetal/service-nodeport.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/mandatory.yaml
