ff61745adc2f5610214ccbff9df7043d76c7bde1
Mark Tagab <proxyshadow@gmail.com>
2020-03-18 00:57:30 +0800

    minor db fix

--- government/src/main/java/me/mtagab/repository/TravelLocalAlertRepository.java
+++ government/src/main/java/me/mtagab/repository/TravelLocalAlertRepository.java
@@ -1,8 +1,9 @@
 package me.mtagab.repository;
 
+import me.mtagab.entity.TravelAlertLocalEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TravelLocalAlertRepository extends CrudRepository<TravelLocalAlertRepository, Long> {
+public interface TravelLocalAlertRepository extends CrudRepository<TravelAlertLocalEntity, Long> {
 }
