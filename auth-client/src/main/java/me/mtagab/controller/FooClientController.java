d35b977a4c9e6da9d676fc70adb49510dad656d5
Mark Tagab <proxyshadow@gmail.com>
2020-03-20 01:11:16 +0800

    working oath2 integration

---
+++ auth-client/src/main/java/me/mtagab/controller/FooClientController.java
@@ -0,0 +1,32 @@
+package me.mtagab.controller;
+
+import me.mtagab.dto.Foo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class FooClientController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/foos/{id}")
+    public String getFooResource(@PathVariable long id, Model model) {
+        Foo foo = restTemplate.getForEntity("http://localhost:9000/resource/foos/" + id, Foo.class).getBody();
+        model.addAttribute("foo", foo);
+        return "foo";
+    }
+
+    @PostMapping("/foos")
+    public String addNewFoo(Foo foo, Model model) {
+        Foo created = restTemplate.postForEntity("http://localhost:9000/resource/foos/", foo, Foo.class).getBody();
+        model.addAttribute("foo", created);
+        return "foo";
+    }
+
+}
---
+++ auth-resource/src/main/java/me/mtagab/config/ResourceServerWebConfig.java
@@ -0,0 +1,13 @@
+package me.mtagab.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({ "com.mtagab.controller" })
+public class ResourceServerWebConfig implements WebMvcConfigurer {
+    //
+}
