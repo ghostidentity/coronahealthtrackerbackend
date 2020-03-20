28ddf65ee4ada4ab88719e1f7b3dde00f57f4629
Mark Tagab <proxyshadow@gmail.com>
2020-03-20 20:06:31 +0800

    updated scripts

--- .config/scripts/pull.sh
+++ .config/scripts/pull.sh
@@ -2,13 +2,13 @@
 
 echo "pulling images..."
 
-docker pull mtagab/gateway:latest
+#docker pull mtagab/gateway:latest
 docker pull mtagab/account:latest
 docker pull mtagab/government:latest
 docker pull mtagab/infection:latest
 docker pull mtagab/notification:latest
-docker pull mtagab/security:latest
-docker pull mtagab/website:latest
+#docker pull mtagab/security:latest
+#docker pull mtagab/website:latest
 docker pull mtagab/person:latest
 docker pull mtagab/hospital:latest
 docker pull mtagab/authentication:latest
--- .config/scripts/push.sh
+++ .config/scripts/push.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 echo "images are now pushing to docker registry .."
-docker push mtagab/gateway
+#docker push mtagab/gateway
 docker push mtagab/account
 docker push mtagab/government
 docker push mtagab/infection
 docker push mtagab/notification
-docker push mtagab/security
 docker push mtagab/hospital
 docker push mtagab/person
-docker push mtagab/website
 docker push mtagab/authentication
 docker push mtagab/resource
 
-docker container prune --force
+#docker container prune --force
