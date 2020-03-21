c754f1f79d050de90792303e9fa2958897530f6b
Mark Tagab <proxyshadow@gmail.com>
2020-03-21 09:47:11 +0800

    update authentication service

--- security/authentication/src/main/java/me/mtagab/AuthorizationServerApplication.java
+++ security/authentication/src/main/java/me/mtagab/AuthorizationServerApplication.java
@@ -1,16 +1,32 @@
 package me.mtagab;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "me.mtagab")
-public class AuthorizationServerApplication extends SpringBootServletInitializer {
+public class AuthorizationServerApplication extends SpringBootServletInitializer implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthorizationServerApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationServerApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("Authentication Service has been started");
+    }
 }
