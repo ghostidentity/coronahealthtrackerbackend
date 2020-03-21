24bef8d2382d0bb8563a4164e237b4bdbd442f17
Mark Tagab <proxyshadow@gmail.com>
2020-03-21 09:29:43 +0800

    bug test

--- security/authentication/src/main/java/me/mtagab/AuthorizationServerApplication.java
+++ security/authentication/src/main/java/me/mtagab/AuthorizationServerApplication.java
@@ -3,7 +3,9 @@ package me.mtagab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "me.mtagab")
 public class AuthorizationServerApplication extends SpringBootServletInitializer {
 
