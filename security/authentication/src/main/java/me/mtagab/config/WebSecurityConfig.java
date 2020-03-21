b0025961e195269180635cd406d9c261a9c2eff8
Mark Tagab <proxyshadow@gmail.com>
2020-03-21 08:00:57 +0800

    updated configuration to allow actuator URL on authentication service via path permission. definition file updated.

--- security/authentication/src/main/java/me/mtagab/config/WebSecurityConfig.java
+++ security/authentication/src/main/java/me/mtagab/config/WebSecurityConfig.java
@@ -44,6 +44,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/login").permitAll()
                 .antMatchers("/oauth/token/revokeById/**").permitAll()
                 .antMatchers("/tokens/**").permitAll()
+                .antMatchers("/actuator/**").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin().permitAll()
                 .and().csrf().disable();
