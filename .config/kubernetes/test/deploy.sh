ef625f2bbeaa6059ce7068bfad85f36c5e83cacc
Mark Tagab <proxyshadow@gmail.com>
2020-03-21 10:23:55 +0800

    refactored

--- .config/kubernetes/test/deploy.sh
+++ .config/kubernetes/test/deploy.sh
@@ -6,3 +6,5 @@ kubectl apply -f healthtracker-secret.yaml
 kubectl apply -f healthtracker-db.yaml
 kubectl apply -f healthtracker-sample.yaml
 kubectl apply -f healthtracker-volume.yaml
+kubectl apply -f healthtracker-authentication.yaml
+kubectl apply -f healthtracker-resource.yaml
