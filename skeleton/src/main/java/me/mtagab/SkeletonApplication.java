4512371679f98aaf2c8dc291a44645d37fedc5ab
Mark Tagab <proxyshadow@gmail.com>
2020-03-27 19:46:21 +0800

    test security

--- skeleton/src/main/java/me/mtagab/SkeletonApplication.java
+++ skeleton/src/main/java/me/mtagab/SkeletonApplication.java
@@ -2,13 +2,14 @@ package me.mtagab;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class SkeletonApplication {
+public class SkeletonApplication implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(SkeletonApplication.class);
 
@@ -16,4 +17,8 @@ public class SkeletonApplication {
         SpringApplication.run(SkeletonApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("Skeleton Service started");
+    }
 }
