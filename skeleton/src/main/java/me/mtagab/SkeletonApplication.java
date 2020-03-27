115db01be0980bd783cfd9822db990f7c8abe128
Mark Tagab <proxyshadow@gmail.com>
2020-03-27 11:06:14 +0800

    Stable Skeleton Service --local exec only

---
+++ skeleton/src/main/java/me/mtagab/SkeletonApplication.java
@@ -0,0 +1,24 @@
+package me.mtagab;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class SkeletonApplication implements CommandLineRunner  {
+
+    private static final Logger logger = LoggerFactory.getLogger(SkeletonApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkeletonApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("Skeleton Service started");
+    }
+}
---
+++ skeleton/src/main/resources/static/precache-manifest.a573c81dc3cc193f0f6dee30c288a6fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4c48e248b66013b137f4b7e393ead3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "525b4db02111cd2743c0",
+    "url": "/static/css/main.28305139.chunk.css"
+  },
+  {
+    "revision": "d67567e863a2253db5df",
+    "url": "/static/js/2.547876dc.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.547876dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "525b4db02111cd2743c0",
+    "url": "/static/js/main.b20a0fe5.chunk.js"
+  },
+  {
+    "revision": "ef9a8d1a763064c87cc2",
+    "url": "/static/js/runtime-main.a468ba33.js"
+  }
+]);
