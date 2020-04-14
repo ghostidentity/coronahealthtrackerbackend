c8ede3bc8d42980bf0584002352ef72b9d46ada4
Mark Tagab <proxyshadow@gmail.com>
2020-04-14 16:59:34 +0800

    include spring profiles

--- skeleton/src/main/java/me/mtagab/controller/IndexController.java
+++ skeleton/src/main/java/me/mtagab/controller/IndexController.java
@@ -1,10 +1,12 @@
 package me.mtagab.controller;
 
+import me.mtagab.dto.Foo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,8 +21,12 @@ public class IndexController {
     private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     @RequestMapping(value = "/api/test", method = RequestMethod.GET)
-    public ResponseEntity<String> HomeController() {
+    public ResponseEntity<String> HomeController( Model model) {
         logger.info("test..");
+
+        Foo foo = restTemplate.getForEntity("http://localhost:9000/resource/foos/", Foo.class).getBody();
+        model.addAttribute("foo", foo);
+
         return ResponseEntity.ok().build();
     }
 
